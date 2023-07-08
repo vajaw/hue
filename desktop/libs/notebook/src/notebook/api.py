317419264f81270bdca9b6e33556d91178aa895c
Amit Srivastava <asrivastava@cloudera.com>
2023-07-08 15:20:59 +0530

    [core] Moved namespaces and computes to database
    The existing implementation was connecting to external Altus and DWX
    api's to fetch the available computes. This commit changes that by
    moving the cluster config to database. Two new tables have been added
    (a) `beeswax_namespace` to hold config for a namespace/dialect
    (b) `beeswax_compute` to hold configs for individual compute clusters
    linked to the namespaces.
    This change currently support hive and impala clusters.
    
    There is also a service-discovery component that keeps the list of
    namespaces and computes updated in the corresponding tables.
    `sync_warehouses.py` performs the service discovery in the CDW environ
    by talking to kubernetes api's. It creates one Hive and one Impala
    namespace and one compute for each virtual warehouse. The command is
    supposed to run every minute and keep the list of warehouses updated.
    
    There are related changes and fixes to the rest of the code to support
    the query execution on different computes.
    
    Change-Id: Ifd8dbc8d716dfe2000fbfa8121e39f2610051fa1

--- desktop/libs/notebook/src/notebook/api.py
+++ desktop/libs/notebook/src/notebook/api.py
@@ -162,7 +162,7 @@ def _execute_notebook(request, notebook, snippet):
         notebook['sessions'] = pre_execute_sessions
 
       # Retrieve and remove the result from the handle
-      if response['handle'].get('sync'):
+      if response['handle'] and response['handle'].get('sync'):
         result = response['handle'].pop('result')
     finally:
       if historify:
@@ -180,7 +180,7 @@ def _execute_notebook(request, notebook, snippet):
             }
             notebook_executable['operationId'] = history.uuid
 
-        if 'handle' in response: # No failure
+        if response.get('handle'): # No failure
           if 'result' not in _snippet: # Editor v2
             _snippet['result'] = {}
           _snippet['result']['handle'] = response['handle']
@@ -750,7 +750,11 @@ def autocomplete(request, server=None, database=None, table=None, column=None, n
 
   # Passed by check_document_access_permission but unused by APIs
   notebook = json.loads(request.POST.get('notebook', '{}'))
-  snippet = json.loads(request.POST.get('snippet', '{}'))
+  cluster = json.loads(request.POST.get('cluster', '{}'))
+  if cluster and cluster.get('type') in ('hive-compute', 'impala-compute'):
+    snippet = cluster
+  else:
+    snippet = json.loads(request.POST.get('snippet', '{}'))
   action = request.POST.get('operation', 'schema')
 
   try:
